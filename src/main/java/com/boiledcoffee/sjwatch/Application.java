7e5afa222f0bff0e9e0bd9b225bab30853f964eb
boildcoffee <1722833435@qq.com>
2018-02-07 15:33:17 +0800

    分页返回总数

--- src/main/java/com/boiledcoffee/sjwatch/Application.java
+++ src/main/java/com/boiledcoffee/sjwatch/Application.java
@@ -3,7 +3,9 @@ package com.boiledcoffee.sjwatch;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by juha on 2018/1/10.
@@ -11,8 +13,13 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan("com.boiledcoffee.sjwatch.servlet")
 @MapperScan("com.boiledcoffee.sjwatch.dao")
-public class Application {
+public class Application extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
